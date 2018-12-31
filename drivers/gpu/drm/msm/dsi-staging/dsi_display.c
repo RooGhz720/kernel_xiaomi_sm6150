191ec4e07438be8cc2063d270ba82bfd9d4f7046
DevriesL <therkduan@gmail.com>
2018-12-31 21:42:21 +0800

    drm: msm: dsi-staging: add get_main_display
    Change-Id: Id670875adc18659521c424a716cf0ef194f9627f

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -111,6 +111,8 @@ void dsi_display_panel_gamma_mode_change(struct dsi_display *display,
 	return;
 }
 
+struct dsi_display *primary_display;
+
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -8138,6 +8140,10 @@ int dsi_display_unprepare(struct dsi_display *display)
 
 	SDE_EVT32(SDE_EVTLOG_FUNC_EXIT);
 	return rc;
+}
+
+struct dsi_display *get_main_display(void) {
+	return primary_display;
 }
 
 static int __init dsi_display_register(void)
