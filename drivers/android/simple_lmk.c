1886539823c5a1377b6106385f74250def4786c3
Yaroslav Furman <yaro330@gmail.com>
2020-01-05 23:00:59 +0300

    simple_lmk: set kthread priority to 7
    It shouldn't be hanging with other RT threads.
    Set it slightly above sde_rot threads.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: PrimoDev23 <lexx.ps2711@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -375,13 +375,14 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 {
 	static atomic_t init_done = ATOMIC_INIT(0);
 	struct task_struct *thread;
+	struct sched_param param = { .sched_priority = 7 };
 
 	if (!atomic_cmpxchg(&init_done, 0, 1)) {
 		thread = kthread_run(simple_lmk_reclaim_thread, NULL,
 				     "simple_lmkd");
 		BUG_ON(IS_ERR(thread));
 		BUG_ON(vmpressure_notifier_register(&vmpressure_notif));
-		BUG_ON(msm_drm_register_client(&fb_notifier_block));
+		sched_setscheduler(thread, SCHED_FIFO, &param);
 	}
 
 	return 0;
