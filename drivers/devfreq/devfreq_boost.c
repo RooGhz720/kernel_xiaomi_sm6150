73b00e34006263a4bb4a629764a7e84c56630710
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-01 18:35:20 +0530

    devfreq_boost: Disable boosting when battery saver is on

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -5,6 +5,7 @@
 
 #define pr_fmt(fmt) "devfreq_boost: " fmt
 
+#include <linux/battery_saver.h>
 #include <linux/devfreq_boost.h>
 #include <linux/fb.h>
 #include <linux/input.h>
@@ -61,7 +62,7 @@ static struct df_boost_drv df_boost_drv_g __read_mostly = {
 
 static void __devfreq_boost_kick(struct boost_dev *b)
 {
-	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || kp_active_mode() == 1)
+	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || is_battery_saver_on())
 		return;
 
 	set_bit(INPUT_BOOST, &b->state);
@@ -86,7 +87,7 @@ static void __devfreq_boost_kick_max(struct boost_dev *b,
 	unsigned long boost_jiffies = msecs_to_jiffies(duration_ms);
 	unsigned long curr_expires, new_expires;
 
-	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || kp_active_mode() == 1)
+	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || is_battery_saver_on())
 		return;
 
 	do {
