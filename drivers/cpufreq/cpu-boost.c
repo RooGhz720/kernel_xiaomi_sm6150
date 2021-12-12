994ad580a92dac94a0cc88c93b0af94d577c40fa
pri0818 <priyanshusinghal0818@gmail.com>
2021-12-12 10:02:13 +0000

    cpu_boost: Don't boost cpu in battery saver mode
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -243,7 +243,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 	u64 now;
 
 
-	if (!input_boost_enabled || is_battery_saver_on())
+	if (!input_boost_enabled)
 		return;
 
 	now = ktime_to_us(ktime_get());
