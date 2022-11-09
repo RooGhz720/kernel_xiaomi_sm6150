b582f4cea06d9ea965ed203c76950e4c5721bd1c
shashank1439 <9945shashank@gmail.com>
2022-11-09 21:19:09 +0530

    Revert "devfreq_boost: Expose disable_boosts node"
    This reverts commit 0f15a7e81f44d79eb2ca39cda1e5821ea116091f.

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -11,11 +11,6 @@
 #include <linux/kthread.h>
 #include <linux/slab.h>
 #include <uapi/linux/sched/types.h>
-#include <drm/drm_panel.h>
-#include <linux/module.h>
-
-static bool disable_boosts __read_mostly;
-module_param(disable_boosts, bool, 0644);
 
 extern int kp_active_mode(void);
 
@@ -74,9 +69,6 @@ static void __devfreq_boost_kick(struct boost_dev *b)
 void devfreq_boost_kick(enum df_device device)
 {
 	struct df_boost_drv *d = &df_boost_drv_g;
-	
-	if (disable_boosts)
-		return;
 
 	__devfreq_boost_kick(d->devices + device);
 }
@@ -109,9 +101,6 @@ static void __devfreq_boost_kick_max(struct boost_dev *b,
 void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
 {
 	struct df_boost_drv *d = &df_boost_drv_g;
-	
-	if (disable_boosts)
-		return;
 
 	__devfreq_boost_kick_max(d->devices + device, duration_ms);
 }
