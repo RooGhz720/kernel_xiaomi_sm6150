26289c2f9a08518b3203ee8208a18816c02eadf3
shashank1439 <9945shashank@gmail.com>
2022-11-09 21:19:06 +0530

    Revert "devfreq: sync devfreq boost driver"
    This reverts commit 627e1a01df4ee6d18fee9c1dfd7072fee794958d.

--- drivers/devfreq/devfreq_devbw.c
+++ drivers/devfreq/devfreq_devbw.c
@@ -25,7 +25,6 @@
 #include <linux/mutex.h>
 #include <linux/interrupt.h>
 #include <linux/devfreq.h>
-#include <linux/devfreq_boost.h>
 #include <linux/slab.h>
 #include <linux/of.h>
 #include <linux/of_fdt.h>
@@ -240,7 +239,7 @@ int devfreq_add_devbw(struct device *dev)
 	}
 
 	if (!strcmp(dev_name(dev), "soc:qcom,cpu-llcc-ddr-bw"))
-		devfreq_register_boost_device(DEVFREQ_CPU_LLCC_DDR_BW, d->df);
+		devfreq_register_boost_device(DEVFREQ_MSM_CPUBW, d->df);
 
 	return 0;
 }
