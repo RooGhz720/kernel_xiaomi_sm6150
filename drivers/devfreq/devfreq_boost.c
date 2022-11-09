9c62552d5ca8e2bf0cb7d9374b326ff39742e8c4
shashank1439 <9945shashank@gmail.com>
2022-11-09 21:19:18 +0530

    Revert "devfreq_boost: convert to drm screen state notifier"
    This reverts commit c1423513de619267f55d505026b0b05c8cad097d.

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -6,10 +6,9 @@
 #define pr_fmt(fmt) "devfreq_boost: " fmt
 
 #include <linux/devfreq_boost.h>
-#include <linux/msm_drm_notify.h>
+#include <linux/fb.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
-#include <linux/slab.h>
 #include <uapi/linux/sched/types.h>
 
 extern int kp_active_mode(void);
@@ -32,7 +31,7 @@ struct boost_dev {
 
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
-	struct notifier_block msm_drm_notif;
+	struct notifier_block fb_notif;
 };
 
 static void devfreq_input_unboost(struct work_struct *work);
@@ -179,23 +178,21 @@ static int devfreq_boost_thread(void *data)
 	return 0;
 }
 
-static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
+static int fb_notifier_cb(struct notifier_block *nb, unsigned long action,
 			  void *data)
 {
-	struct df_boost_drv *d = container_of(nb, typeof(*d), msm_drm_notif);
-	int i;
-	struct msm_drm_notifier *evdata = data;
-	int *blank = evdata->data;
+	struct df_boost_drv *d = container_of(nb, typeof(*d), fb_notif);
+	int i, *blank = ((struct fb_event *)data)->data;
 
 	/* Parse framebuffer blank events as soon as they occur */
-	if (action != MSM_DRM_EARLY_EVENT_BLANK)
+	if (action != FB_EARLY_EVENT_BLANK)
 		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
 	for (i = 0; i < DEVFREQ_MAX; i++) {
 		struct boost_dev *b = d->devices + i;
 
-		if (*blank == MSM_DRM_BLANK_UNBLANK) {
+		if (*blank == FB_BLANK_UNBLANK) {
 			clear_bit(SCREEN_OFF, &b->state);
 			__devfreq_boost_kick_max(b,
 				CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS);
@@ -317,9 +314,9 @@ static int __init devfreq_boost_init(void)
 		goto stop_kthreads;
 	}
 
-	d->msm_drm_notif.notifier_call = msm_drm_notifier_cb;
-	d->msm_drm_notif.priority = INT_MAX;
-	ret = msm_drm_register_client(&d->msm_drm_notif);
+	d->fb_notif.notifier_call = fb_notifier_cb;
+	d->fb_notif.priority = INT_MAX;
+	ret = fb_register_client(&d->fb_notif);
 	if (ret) {
 		pr_err("Failed to register fb notifier, err: %d\n", ret);
 		goto unregister_handler;
