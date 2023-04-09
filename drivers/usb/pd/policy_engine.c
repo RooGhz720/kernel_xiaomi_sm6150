0a778a32b476141e3cfac91f4d81c01a2b004e5c
Pranav Vashi <neobuddy89@gmail.com>
2023-04-09 09:42:42 +0530

    usb: pd: Add few safe-check fixes
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/usb/pd/policy_engine.c
+++ drivers/usb/pd/policy_engine.c
@@ -2709,6 +2709,7 @@ enable_reg:
 	if (!pd->vbus) {
 		pd->vbus = devm_regulator_get(pd->dev.parent, "vbus");
 		if (IS_ERR(pd->vbus)) {
+			pd->vbus = NULL;
 			usbpd_err(&pd->dev, "Unable to get vbus\n");
 			return -EAGAIN;
 		}
@@ -3409,6 +3410,7 @@ static void usbpd_sm(struct work_struct *w)
 			memcpy(&pd->src_cap_ext_db, rx_msg->payload,
 				sizeof(pd->src_cap_ext_db));
 			complete(&pd->is_ready);
+			break;
 		} else if (IS_EXT(rx_msg, MSG_PPS_STATUS)) {
 			if (rx_msg->data_len != sizeof(pd->pps_status_db)) {
 				usbpd_err(&pd->dev, "Invalid pps status db\n");
@@ -3417,6 +3419,7 @@ static void usbpd_sm(struct work_struct *w)
 			memcpy(&pd->pps_status_db, rx_msg->payload,
 				sizeof(pd->pps_status_db));
 			complete(&pd->is_ready);
+			break;
 		} else if (IS_EXT(rx_msg, MSG_STATUS)) {
 			if (rx_msg->data_len != PD_STATUS_DB_LEN) {
 				usbpd_err(&pd->dev, "Invalid status db\n");
@@ -3426,6 +3429,7 @@ static void usbpd_sm(struct work_struct *w)
 				sizeof(pd->status_db));
 			kobject_uevent(&pd->dev.kobj, KOBJ_CHANGE);
 			complete(&pd->is_ready);
+			break;
 		} else if (IS_EXT(rx_msg, MSG_BATTERY_CAPABILITIES)) {
 			if (rx_msg->data_len != PD_BATTERY_CAP_DB_LEN) {
 				usbpd_err(&pd->dev, "Invalid battery cap db\n");
@@ -3434,6 +3438,7 @@ static void usbpd_sm(struct work_struct *w)
 			memcpy(&pd->battery_cap_db, rx_msg->payload,
 				sizeof(pd->battery_cap_db));
 			complete(&pd->is_ready);
+			break;
 		} else if (IS_EXT(rx_msg, MSG_BATTERY_STATUS)) {
 			if (rx_msg->data_len != sizeof(pd->battery_sts_dobj)) {
 				usbpd_err(&pd->dev, "Invalid bat sts dobj\n");
@@ -3442,6 +3447,7 @@ static void usbpd_sm(struct work_struct *w)
 			memcpy(&pd->battery_sts_dobj, rx_msg->payload,
 				sizeof(pd->battery_sts_dobj));
 			complete(&pd->is_ready);
+			break;
 		} else if (IS_CTRL(rx_msg, MSG_GET_SOURCE_CAP_EXTENDED)) {
 			handle_get_src_cap_extended(pd);
 		} else if (IS_EXT(rx_msg, MSG_GET_BATTERY_CAP)) {
@@ -5038,6 +5044,10 @@ static void usbpd_mi_connect_cb(struct usbpd_svid_handler *hdlr, bool peer_usb_c
 	struct usbpd *pd;
 
 	pd = container_of(hdlr, struct usbpd, svid_handler);
+	if(!pd) {
+		pr_err("get_usbpd phandle failed\n");
+		return;
+	}
 
 	pd->uvdm_state = USBPD_UVDM_CONNECT;
 	usbpd_info(&pd->dev, "hdlr->svid:%x has connect, support peer_usb_comm = %d\n", hdlr->svid, peer_usb_comm);
@@ -5049,6 +5059,10 @@ static void usbpd_mi_disconnect_cb(struct usbpd_svid_handler *hdlr)
 	struct usbpd *pd;
 
 	pd = container_of(hdlr, struct usbpd, svid_handler);
+	if(!pd) {
+		pr_err("get_usbpd phandle failed\n");
+		return;
+	}
 
 	pd->adapter_id = 0;
 	pd->adapter_svid = 0;
@@ -5069,6 +5083,11 @@ static void usbpd_mi_vdm_received_cb(struct usbpd_svid_handler *hdlr, u32 vdm_hd
 	int ret;
 
 	pd = container_of(hdlr, struct usbpd, svid_handler);
+	if(!pd) {
+		pr_err("get_usbpd phandle failed\n");
+		return;
+	}
+
 	cmd = UVDM_HDR_CMD(vdm_hdr);
 
 	usbpd_dbg(&pd->dev, "hdlr->svid:0x%04x, vdm_hdr:0x%08x, num_vdos:%d, cmd:%d\n", hdlr->svid, vdm_hdr, num_vdos, cmd);
