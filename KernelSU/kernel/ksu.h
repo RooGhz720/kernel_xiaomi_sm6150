ff2d53b8a9d2c78252a82447b83c14ad525bd6f9
RooGhz720 <Erzatp720@gmail.com>
2023-08-20 08:12:59 +0800

    ksu: update and sync
    Signed-off-by: RooGhz720 <Erzatp720@gmail.com>

--- KernelSU/kernel/ksu.h
+++ KernelSU/kernel/ksu.h
@@ -4,7 +4,7 @@
 #include "linux/types.h"
 #include "linux/workqueue.h"
 
-#define KERNEL_SU_VERSION 11094
+#define KERNEL_SU_VERSION 11200
 #define KERNEL_SU_OPTION 0xDEADBEEF
 
 #define CMD_GRANT_ROOT 0
